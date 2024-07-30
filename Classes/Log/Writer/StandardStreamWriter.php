07114f397fceef80448e8bebdde8190748de6359
Martin Adler <mteu@mailbox.org>
2024-07-30 20:13:35 +0200

    [TASK] Add Rector config

--- Classes/Log/Writer/StandardStreamWriter.php
+++ Classes/Log/Writer/StandardStreamWriter.php
@@ -37,7 +37,7 @@ use TYPO3\CMS\Core\Log\Writer\WriterInterface;
  */
 final class StandardStreamWriter extends AbstractWriter
 {
-    private StandardStream $stdStream;
+    private readonly StandardStream $stdStream;
 
     /**
      * @param array{stdStream: StandardStream} $options
--- rector.php
+++ rector.php
@@ -21,8 +21,22 @@ declare(strict_types=1);
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-$config = \TYPO3\CodingStandards\CsFixerConfig::create();
-$config->setParallelConfig(\PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect());
-$config->getFinder()->in(__DIR__);
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\ValueObject\PhpVersion;
+use Ssch\TYPO3Rector\Set\Typo3LevelSetList;
 
-return $config;
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/Classes',
+        __DIR__ . '/Configuration',
+        __DIR__ . '/Tests',
+    ]);
+
+    $rectorConfig->phpVersion(PhpVersion::PHP_81);
+
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_81,
+        Typo3LevelSetList::UP_TO_TYPO3_12,
+    ]);
+};
