df0806bb860bccc0ecbad547cc5874f181c48e00
Martin Adler <mteu@mailbox.org>
2024-07-30 20:01:27 +0200

    [TASK] Update PHPStan config

--- phpstan.php
+++ phpstan.php
@@ -2,17 +2,41 @@
 
 declare(strict_types=1);
 
-return [
+$configuration = [
     'parameters' => [
-        'level' => 'max',
-        'paths' => [
-            'Classes',
-            'Configuration',
-            'Tests',
-        ],
-        'docblock' => [
-            'copyrightIdentifier' => 'Copyright (C)',
-            'requiredLicenseIdentifier' => 'GPL-3.0',
+        'ignoreErrors' => [],
+    ],
+];
+
+$configuration = require 'phpstan-baseline.php';
+
+$configuration['parameters']['level'] = 'max';
+
+$configuration['parameters']['paths'] = [
+    'Classes',
+    'Configuration',
+    'Tests',
+];
+
+$configuration['parameters']['docblock'] = [
+    'copyrightIdentifier' => 'Copyright (C) ',
+    'requiredLicenseIdentifier' => 'GPL-3.0',
+];
+
+$configuration['parameters']['ergebnis'] = [
+    'noExtends' => [
+        'classesAllowedToBeExtended' => [
+            \TYPO3\CMS\Core\Log\Writer\AbstractWriter::class,
         ],
     ],
 ];
+
+if (count($configuration['parameters']['ignoreErrors']) > 0) {
+    foreach ($configuration['parameters']['ignoreErrors'] as $baselineIgnore) {
+        $ignoreErrors[] = $baselineIgnore['message'];
+    }
+}
+
+$configuration['parameters']['ignoreErrors'] = array_unique($ignoreErrors);
+
+return $configuration;
