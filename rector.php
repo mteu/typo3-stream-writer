abc22eeede53c5b574d2f5bf59dab9473eb7ad51
Martin Adler <mteu@mailbox.org>
2024-12-27 10:00:03 +0100

    [FEATURE] Drop PHP 8.1 support

--- rector.php
+++ rector.php
@@ -33,10 +33,10 @@ return static function (RectorConfig $rectorConfig): void {
         __DIR__ . '/Tests',
     ]);
 
-    $rectorConfig->phpVersion(PhpVersion::PHP_81);
+    $rectorConfig->phpVersion(PhpVersion::PHP_82);
 
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_81,
+        LevelSetList::UP_TO_PHP_82,
         Typo3LevelSetList::UP_TO_TYPO3_12,
     ]);
 };
