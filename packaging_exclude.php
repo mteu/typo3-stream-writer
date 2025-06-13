5163afc16fcec072b51d9dd42acb59cd52bb442f
Martin Adler <mteu@mailbox.org>
2025-06-13 15:04:38 +0200

    [TASK] Publish extension to TYPO3 Extension Repository (TER)

---
+++ packaging_exclude.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'directories' => [
+        '.build',
+        '.git',
+        '.github',
+        'Tests',
+    ],
+    'files' => [
+        'composer.lock',
+        'editorconfig',
+        'gitattributes',
+        'gitignore',
+        'packaging_exclude.php',
+        'php-cs-fixer.php',
+        'phpstan.neon',
+        'phpstan-baseline.neon',
+        'phpunit.functional.xml',
+        'phpunit.unit.xml',
+        'rector.php',
+        'renovate.json',
+        'version-bumper.yaml',
+    ],
+];
